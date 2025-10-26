b1f90f2503bed6680ed7406a33ec63a5d6f15830
InaNat <ina.natseva@icloud.com>
2025-10-25 18:20:09 -0700

    more camera fix

--- pipi/main.py
+++ pipi/main.py
@@ -14,12 +14,14 @@ def main():
 
         # Once heard, open a camera stream using picamera2
         picam2 = Picamera2()
-        config = picam2.create_still_configuration(main={"format": "RGB888", "size": (4056, 3040)})
+        config = picam2.create_video_configuration(main={"format": "RGB888", "size": (1920, 1080)}, controls={"FrameDurationLimits": (33333, 33333)})
         picam2.configure(config)
-        picam2.set_controls({"AfMode": 1, "AeEnable": True, "AwbEnable": True})
+        picam2.set_controls({"AfMode": 2, "AeEnable": True, "AwbEnable": True})
         picam2.start()
 
         print("✅ Camera stream started. Press 'q' to quit.")
+        cv2.namedWindow("Camera Stream", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("Camera Stream", 960, 540)
         while True:
             frame = picam2.capture_array()
             cv2.imshow("Camera Stream", frame)
